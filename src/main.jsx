26ca7ed1234a6bc880cbe72226aae930d1c78773
lakshminarayanan-45 <lakshminarayanan45074507@gmail.com>
2025-12-16 14:17:25 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,24 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
+import keycloak from "./keycloak";
 
-createRoot(document.getElementById("root")).render(<App />);
+keycloak
+  .init({
+    onLoad: "login-required",
+    pkceMethod: "S256",
+    checkLoginIframe: false,
+  })
+  .then((authenticated) => {
+    if (!authenticated) {
+      window.location.reload();
+      return;
+    }
+
+    createRoot(document.getElementById("root")).render(
+      <App keycloak={keycloak} />
+    );
+  })
+  .catch((error) => {
+    console.error("Keycloak initialization failed:", error);
+  });
